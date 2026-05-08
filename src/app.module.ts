84e77ec2ee4c4987fd44c3562d4caaa80d46039e
milad998 <myaralbwd20@gmail.com>
2026-05-08 14:36:19 +0300

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TransfersModule } from './transfers/transfers.module';
@@ -12,29 +12,16 @@ import { AuditModule } from './audit/audit.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
-  type: 'postgres',
-  url: process.env.DATABASE_URL,
-  entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  synchronize: true,
-  ssl: { rejectUnauthorized: false },
-  extra: {
-    family: 6,
-  },
-}),
-
-    AuthModule,
-    UsersModule,
-    TransfersModule,
-    NotificationsModule,
-    WalletModule,
-    FavoritesModule,
-    ReportsModule,
-    AuditModule,
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      ssl: { rejectUnauthorized: false },
+    }),
+    AuthModule, UsersModule, TransfersModule, NotificationsModule,
+    WalletModule, FavoritesModule, ReportsModule, AuditModule,
   ],
 })
 export class AppModule {}
